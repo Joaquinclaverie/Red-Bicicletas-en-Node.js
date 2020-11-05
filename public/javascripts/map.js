bf3f332bf5de33bd51b3ca8d5a833f94b507e206
jclaverie <jclaverieb@gmail.com>
2020-11-05 17:58:16 -0300

    first commit

---
+++ public/javascripts/map.js
@@ -0,0 +1,18 @@
+var mymap = L.map('ancho').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+}).addTo(mymap);
+
+
+/*
+$.ajax({
+    dataType: "json",
+    url: "api/bicicletas",
+    success: function(result){
+        console.log(result);
+        result.bicicletas.forEach(function(bici){
+            L.marker(bici.ubicacion, {title: bici.id}).addTo(map);
+        }
+    }
+})
