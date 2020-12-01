153760bdab0ee649a99017616600739ca614ce51
jclaverie <jclaverieb@gmail.com>
2020-11-30 23:46:05 -0300

    agregue sendgrid

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,16 +1,40 @@
 const nodemailer = require('nodemailer');
+const sgTransport = require('nodemailer-sendgrid-transport');
 
-const mailConfig = {
-    host: 'smtp.ethereal.email',
-    secure: false,
-    port: 587,
-    auth: {
-        user: 'hassan.frami@ethereal.email',
-        pass: 'gJjBpQQsVhK4j8KU7p'
-    },
-    tls: {
-        rejectUnauthorized: false
+let mailConfig;
+if (process.env.NODE_ENV === 'production' ){
+    const options = {
+        auth: {
+            api_key: process.env.SENDGRID_API_SECRET
+        }
     }
-};
+    mailConfig = sgTransport(options);
+
+} else {
+    if (process.env.NODE_ENV === 'staging' ){
+        console.log('XXXXXXXXXXXXX');
+        const options = {
+            auth: {
+                api_key: process.env.SENDGRID_API_SECRET
+            }
+        }
+        mailConfig = sgTransport(options);
+
+    } else {
+        // all emails are catched by ethereal.email
+        mailConfig = {
+            host: 'smtp.ethereal.email',
+            secure: false,
+            port: 587,
+            auth: {
+                user: process.env.ethereal_user,
+                pass: process.env.ethereal_pwd
+            },
+            tls: {
+                rejectUnauthorized: false
+            }
+        };
+    }
+}
 
 module.exports = nodemailer.createTransport(mailConfig);
