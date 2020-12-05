9597c4ef85b175b21d16e07073bc744607610165
Joaquinclaverie <jclaverieb@gmail.com>
2020-12-05 05:59:36 -0300

    Finalizando la Semana 4 del Curso

--- newrelic.js
+++ newrelic.js
@@ -13,7 +13,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key: 'license key here',
+  license_key: '01231231231233',
   /**
    * This setting controls distributed tracing.
    * Distributed tracing lets you see the path that a request takes through your
